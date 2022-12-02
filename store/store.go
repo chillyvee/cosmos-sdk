c4a512a40566e991a18ba3a7ac05cb99888b22b5
Chill Validation <blockhead@chillvalidation.com>
2022-12-03 00:03:43 +0100

    CV Restore default rootmulti NopLogger

--- store/store.go
+++ store/store.go
@@ -1,8 +1,6 @@
 package store
 
 import (
-	"os"
-
 	"github.com/tendermint/tendermint/libs/log"
 	dbm "github.com/tendermint/tm-db"
 
@@ -12,15 +10,7 @@ import (
 )
 
 func NewCommitMultiStore(db dbm.DB) types.CommitMultiStore {
-	// Developers may enable rootmulti logging by setting env LOG_ROOTMULTI=1
-	if len(os.Getenv("LOG_ROOTMULTI")) > 0 {
-		// Enable rootmulti logging
-		tmlogger := log.NewTMLogger(os.Stderr).With("module", "rootmulti")
-		return rootmulti.NewStore(db, tmlogger)
-	} else {
-		// Original design disables all rootmulti logging
-		return rootmulti.NewStore(db, log.NewNopLogger())
-	}
+	return rootmulti.NewStore(db, log.NewNopLogger())
 }
 
 func NewCommitKVStoreCacheManager() types.MultiStorePersistentCache {
