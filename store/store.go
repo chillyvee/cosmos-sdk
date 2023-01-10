b6c992d4b9f04ef5d2e4cd4a86213f9efa700f7d
chillyvee <blockhead@chillvalidation.com>
2023-01-10 18:42:43 +0100

    CV LOG_ROOTMULTI

--- store/store.go
+++ store/store.go
@@ -1,6 +1,7 @@
 package store
 
 import (
+	"os"
 	"github.com/tendermint/tendermint/libs/log"
 	dbm "github.com/tendermint/tm-db"
 
@@ -10,8 +11,17 @@ import (
 )
 
 func NewCommitMultiStore(db dbm.DB) types.CommitMultiStore {
-	return rootmulti.NewStore(db, log.NewNopLogger())
+	// Developers may enable rootmulti logging by setting env LOG_ROOTMULTI=1
+        if len(os.Getenv("LOG_ROOTMULTI")) > 0 {
+               // Enable rootmulti logging
+               tmlogger := log.NewTMLogger(os.Stderr).With("module", "rootmulti")
+               return rootmulti.NewStore(db, tmlogger)
+        } else {
+               // Original design disables all rootmulti logging
+               return rootmulti.NewStore(db, log.NewNopLogger())
+        }
 }
+
 
 func NewCommitKVStoreCacheManager() types.MultiStorePersistentCache {
 	return cache.NewCommitKVStoreCacheManager(cache.DefaultCommitKVStoreCacheSize)
