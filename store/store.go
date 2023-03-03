89223a340221e3f9eb6b4144b0f276c9956bcf30
Chill Validation <blockhead@chillvalidation.com>
2023-03-03 22:57:21 +0100

    CV: Optional Logger

--- store/store.go
+++ store/store.go
@@ -1,6 +1,8 @@
 package store
 
 import (
+	"os"
+
 	"github.com/tendermint/tendermint/libs/log"
 	dbm "github.com/tendermint/tm-db"
 
@@ -10,7 +12,15 @@ import (
 )
 
 func NewCommitMultiStore(db dbm.DB) types.CommitMultiStore {
-	return rootmulti.NewStore(db, log.NewNopLogger())
+	// Developers may enable rootmulti logging by setting env LOG_ROOTMULTI=1
+	if len(os.Getenv("LOG_ROOTMULTI")) > 0 {
+		// Enable rootmulti logging
+		tmlogger := log.NewTMLogger(os.Stderr).With("module", "rootmulti")
+		return rootmulti.NewStore(db, tmlogger)
+	} else {
+		// Original design disables all rootmulti logging
+		return rootmulti.NewStore(db, log.NewNopLogger())
+	}
 }
 
 func NewCommitKVStoreCacheManager() types.MultiStorePersistentCache {
